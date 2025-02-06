eafdb404779575bdc0020c8324d33efe85618f3e
KristinaFinck <kristina.finck83@gmail.com>
2025-02-06 23:14:31 +0200

    error refactoring

--- src/components/routes/ProtectedRoute.tsx
+++ src/components/routes/ProtectedRoute.tsx
@@ -1,11 +1,12 @@
 import {ReactNode} from 'react';
 import {Navigate} from "react-router-dom";
+import {Error404} from "../pages/Error404";
 
 type Props = {
     children: ReactNode
 };
 export const ProtectedRoute = ({children}: Props): JSX.Element => {
-    const logged = true // Здесь должна быть реальная проверка авторизации
+    const logged = false // Здесь должна быть реальная проверка авторизации
 
-    return logged ? <>{children}</> : <Navigate to={'/error'} />
+    return logged ? <>{children}</> : <Navigate to="/error" replace />;
 };
